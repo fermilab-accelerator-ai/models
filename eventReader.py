2790e0e586f1ed6e9c2308358844963f64f9345c
Rachael Keller <rachaeltara.keller@gmail.com>
2019-06-21 12:50:53 -0500

    updating code to mkdir of data

--- eventReader.py
+++ eventReader.py
@@ -1,7 +1,7 @@
 # Quick script to pull data on TLG.
 # NOTE: This script is VERY specific to the html being parsed.
 #       The TLG is updated every 60 seconds
-import sys
+import os, sys
 if sys.version_info[0] < 3: 
     from StringIO import StringIO
 else:
@@ -15,65 +15,58 @@ import time
 import datetime
 from apscheduler.schedulers.background import BackgroundScheduler
  
-REFRESH_INTERVAL = 60 # seconds
-RESET_INTERVAL   = 121 # seconds
+
+REFRESH_INTERVAL = 61 # seconds
+RESET_INTERVAL   = REFRESH_INTERVAL*1+1 # 15 minutes and change (<60) for reset
 
 scheduler = BackgroundScheduler()
 scheduler.start()
 
+
 # FUNCTION: pull_tlg()
-# INPUT: df ~ pd.dataFrame
 # OUTPUT: void, concats dataframes with new data on TLG
 # PURPOSE: function to dump data
-def pull_tlg(df):
+def pull_tlg(test_dir):
 	url = 'http://www-ad.fnal.gov/cgi-bin/acl.pl?acl=tlg_info/all_event_info'
 	response = requests.get(url)
+	t = datetime.datetime.now();
 
+	# pull text
 	soup = BeautifulSoup(response.text, "html.parser")
 	str1 = soup.get_text()
+
+	# format text so we can iterate over it
 	str2 = str1.replace('  ',' ').replace('Main Injector','MI').replace('State ID','').replace('Time','').replace('Machine','').replace('Event','')
-	str2=str2.replace(') ',',').replace(' :  ',',').replace(' : ',',').replace(' ',',')
+	str2=str2.replace(' :  ',',').replace(' : ',',').replace(') ',' ,')
 	str2=str2.replace(',,,',',').replace(',,',',')
-	str2=str2.replace('Event','')
 	str3=str2.split('\n')
 
-	df2 = pd.read_csv(StringIO('\n'.join(str3[1:-1])), sep = ',', header=None)
-	df2.columns = ['n','na','Event','State ID','Time','Machine']
-	df2=df2.drop('na',axis=1)
-	df= pd.concat([df,df2])
-	return df
-
-
-# FUNCTION: build_buffer
-# INPUT: df ~ pd.dataFrame
-# OUTPUT: void, outputs csv of dataframe to local directory
-# PURPOSE: function to dump data
-def build_buffer(df):
+	# create dataframe from text
+	df = pd.read_csv(StringIO('\n'.join(str3[1:-1])), sep = ',', header=None)
+	df.columns = ['na','Event','State ID','Time','Machine']
+	df=df.drop('na',axis=1)
+	# convert the seconds to the time logged from pull
+	df['Time']=df['Time'].apply(lambda x: (pd.to_timedelta(pd.to_numeric(x), unit ='s')+t).strftime("%d-%b-%Y %H:%M:%S.%f"))
 	time_stamp=time.strftime("%Y%m%d%H%M%S", time.gmtime())
-	df.to_csv('TLG_'+time_stamp+'.csv')
-	df=pd.DataFrame()
+
+	# dump to csv
+	df.to_csv(test_dir +'/TLG_'+time_stamp+'.csv')
 
 # FUNCTION: main()
 # PURPOSE: run scheduled tasks
 def main():
-	url = 'http://www-ad.fnal.gov/cgi-bin/acl.pl?acl=tlg_info/all_event_info'
-	response = requests.get(url)
 
-	soup = BeautifulSoup(response.text, "html.parser")
-	str1 = soup.get_text()
-	str2 = str1.replace('  ',' ').replace('Main Injector','MI').replace('State ID','').replace('Time','').replace('Machine','').replace('Event','')
-	str2=str2.replace(') ',',').replace(' :  ',',').replace(' : ',',').replace(' ',',')
-	str2=str2.replace(',,,',',').replace(',,',',')
-	str2=str2.replace('Event','')
-	str3=str2.split('\n')
+	time_stamp=time.strftime("%Y%m%d%H%M%S", time.gmtime())
+	test_dir = 'test_' + time_stamp
+	os.makedirs(test_dir)
 
-	df = pd.read_csv(StringIO('\n'.join(str3[1:-1])), sep = ',', header=None)
-	df.columns = ['n','na','Event','State ID','Time','Machine']
-	df=df.drop('na',axis=1)
+	# while not os.path.exists(test_dir):
+	# 	os.makedirs(test_dir)
+	# 	print("Directory " , test_dir ,  " Created ")
+	# else:
+	# 	test_dir = tests + str(counter) 
 
-	scheduler.add_job(func=pull_tlg,trigger='interval',seconds = REFRESH_INTERVAL,args=[df])
-	scheduler.add_job(func=build_buffer,trigger='interval',seconds = RESET_INTERVAL,args=[df])
-
+	scheduler.add_job(func=pull_tlg,trigger='interval',seconds = REFRESH_INTERVAL,args=[test_dir])
 	while True:
 		time.sleep(1)
 
